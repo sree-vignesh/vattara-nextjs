136346034c55764d43322f6edd567f1e8102f03b
sree-vignesh <sreevignesh256@gmail.com>
2025-11-04 13:22:42 +0530

    Dependency and config changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "vattara-ad.s3.us-east-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "vattara-user-image.s3.us-east-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "vattara-img.s3.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
