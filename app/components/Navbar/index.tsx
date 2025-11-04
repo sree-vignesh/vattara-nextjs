3994b57b310e97b4a3fe38d0a8c6939dc16a8c77
sree-vignesh <sreevignesh256@gmail.com>
2025-11-04 10:47:13 +0530

    Navbar Structure

---
+++ app/components/Navbar/index.tsx
@@ -0,0 +1,27 @@
+"use client"; // allows using interactivity (optional if no JS logic)
+
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-white text-black p-4 flex justify-around items-center">
+      <h1 className="text-xl font-bold text-blue-600">Vattara</h1>
+
+      <div className="flex space-x-6">
+        <Link href="/properties">Properties</Link>
+        <Link href="/services">Services</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/About">About</Link>
+      </div>
+
+      <div className="flex space-x-3">
+        <button className="bg-transparent text-blue-600 px-4 py-2 rounded font-semibold">
+          Login
+        </button>
+        <button className="bg-blue-600 text-white px-4 py-2 rounded font-semibold">
+          Sign Up
+        </button>
+      </div>
+    </nav>
+  );
+}
